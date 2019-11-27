97d025763d63ebcfc31e5e289e6dfe23b36555e7
daijinwu <daijinwu@vipkid.com.cn>
2019-11-27 17:30:09 +0800

    bm 详解

--- src/main/java/kmp/BoyerMoore.java
+++ src/main/java/kmp/BoyerMoore.java
@@ -11,6 +11,37 @@ public class BoyerMoore {
 
     private int SIZE = 256;
 
+    public static void main(String[] args) {
+        BoyerMoore bm = new BoyerMoore();
+        bm.BM("bcababab", "bcabababbcababab");
+    }
+
+    private void BM(String pattern, String text) {
+        int i,j = 0;
+        int m = pattern.length();
+        int n = text.length();
+        int[] bmBc = new int[SIZE];
+        int[] bmGs = new int[m];
+        //处理坏字符算法
+        preBmBc(pattern, m, bmBc);
+        System.out.println("c = " + bmBc['c']);
+        //处理好后缀算法
+        preBmGs(pattern, m, bmGs);
+
+        while (j <= n - m) {
+            for (i = m - 1;i >= 0 && pattern.charAt(i) == text.charAt(i + j); --i){}
+            if (i < 0) {
+                System.out.println(" find it index: " + j);
+                return;
+            } else {
+                j += Math.max(bmGs[i], bmBc[text.charAt(i)] - (m - 1 -i));
+            }
+
+        }
+
+        System.out.println("result is not find");
+    }
+
     /**
      * 计算坏字符数组bmBc[]
      * 如果字符在模式串中出现，bmBc['v'] 表示字符v在模式中最后一次出现的位置，距离模式串串尾的长度
@@ -36,26 +67,35 @@ public class BoyerMoore {
         //计算后缀数组
         suffix(pattern, m , suff);
 
+        System.out.println("----------- ");
+        for (int k = 0;k<m;k++) {
+            System.out.println("k = " + k + "\t value = " + suff[k]);
+        }
+        System.out.println("----------- ");
+
         /**
-         * 先赋值为m，包含case3
+         * 全部初始为自己的长度，处理第三种情况
          */
         for (i = 0;i<m;i++) {
             bmGs[i] = m;
         }
 
-        // case2
+        // 处理第二种情况
         j = 0;
         for (i = m - 1;i>=0;i--) {
+            //找到合适位置
             if (suff[i] == i+1) {
-                for (;j < m-1-i;j++) {
+                for (;j<m-1-i;j++) {
+                    //保证每个位置最多只能被修改一次
                     if (bmGs[j] == m) {
-                        bmGs[j] = m - 1-i;
+                        bmGs[j] = m-1-i;
                     }
                 }
             }
         }
-        //case1
-        for (i = 0; i<m-2;i++) {
+
+        //处理第一种情况，顺序是从前到后
+        for (i = 0; i<m-1;i++) {
             bmGs[m-1-suff[i]] = m-1-i;
         }
 

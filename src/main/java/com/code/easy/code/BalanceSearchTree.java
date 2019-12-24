51143a2345880521b6d5877c0bc372a780b9c312
daijinwu <daijinwu@vipkid.com.cn>
2019-12-25 00:07:48 +0800

    有序数组转换为二叉搜索树

--- src/main/java/com/code/easy/code/BalanceSearchTree.java
+++ src/main/java/com/code/easy/code/BalanceSearchTree.java
@@ -144,4 +144,30 @@ public class BalanceSearchTree {
         balanceSearchTree.inOrder(balanceSearchTree.root);
     }
 
+    /**
+     * 递归实现:
+     * 按照二分思想：中点左边的数作为左子树，中点右边的数作为右子树。
+     * @param nums nums
+     * @return  TreeNode
+     */
+    public TreeNode sortedArrayToBST2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return help(nums, 0, nums.length - 1);
+    }
+
+
+    private TreeNode help(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(nums[mid], null, null);
+        node.left = help(nums, start, mid - 1);
+        node.right = help(nums, mid + 1, end);
+        return node;
+    }
+
+
 }

4c79aa59170a232d9f0d07096c30a0fdbaad3a76
daijinwu <daijinwu@vipkid.com.cn>
2019-12-30 19:27:49 +0800

    xx

---
+++ src/main/java/com/code/easy/code/Generate_118.java
@@ -0,0 +1,13 @@
+package com.code.easy.code;
+
+import java.util.List;
+
+public class Generate_118 {
+    public static List<List<Integer>> generate(int nums) {
+
+        return null;
+    }
+    public static void main(String[] args) {
+
+    }
+}

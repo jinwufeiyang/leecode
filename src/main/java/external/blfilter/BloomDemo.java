ae59bb16ce5029426c4afdb47946e55ccea77435
daijinwu <daijinwu@vipkid.com.cn>
2020-01-08 20:39:28 +0800

    布隆1.9

--- src/main/java/external/blfilter/BloomDemo.java
+++ src/main/java/external/blfilter/BloomDemo.java
@@ -7,8 +7,10 @@ public class BloomDemo {
 
     public static void main(String[] args) {
         BloomFilter filter = BloomFilter.create(Funnels.integerFunnel(), 100000, 0.01);
-        for (int i=0; i<1000;i++) {
+        for (int i=0; i<100;i++) {
             filter.put(i);
         }
+        System.out.println(filter.mightContain(3));
+        System.out.println("执行完毕!");
     }
 }
